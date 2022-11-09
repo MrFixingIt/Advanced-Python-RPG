a98d04f94321a919fe637c16d7056c2368516729
MrFixingIt <daryltwaltz423@gmail.com>
2022-11-08 18:30:20 -0600

    PreFight Story Complete

--- main.py
+++ main.py
@@ -34,6 +34,76 @@ enemy_three = {
     "coins": {"copper": 12, "silver": 16, "gold": 20}
 }
 
+print("------------------------------ Narrator ------------------------------")
+print("As our hero, Garen, spawns in Runeterra to start yet another adventure,")
+print("he turns to the shopkeeper to purchase his starter gear.")
+print("He gets suited up and gazes off into the distance while thinking to himself...")
+print("Should I go into the jungle?... Maybe I can go suppot ADC...")
+print("I think I'll go top today.")
+print("Our hero takes a deep breath, gazes upon the enemy nexus off in the distance,")
+print("readies his sword and starts on his journey towards top lane...")
+print("Our hero makes it out of the base and about half way accross the field without incident")
+print("until he hears a voice from the jungle.")
+
+print(" ------------------------------ Cannon Minion ------------------------------ ")
+print("Hey you!... Yea you!")
+
+print("------------------------------ Narrator ------------------------------")
+print("Garen stops and looks to his right side toward the sound of the voice...")
+
+print(" ------------------------------ Garen ------------------------------ ")
+print("Who goes there! Don't waste my time! Show yourself!")
+
+print("------------------------------ Narrator ------------------------------")
+print("Garen, isn't too concerned but he is always cautions.")
+print("He knows the rift is a dangerous place. For this reason, he never completely lets his guard down.")
+print("Damacian warriors are feared and respected accross all lands for their raw strength, fearlessness")
+print("and sharp, heavy weapons.")
+print("A cannon minion appears from the darkness of the jungle.")
+
+print(" ------------------------------ Cannon Minion ------------------------------ ")
+print("HAHAHAHA... It's not safe out here! You shouldn't be out here alone.")
+
+print("------------------------------ Narrator ------------------------------")
+print("Suddenly, a warning shot is fired from a wand from the bushes behind Garen!")
+print("Garen doesn't even flinch as the shot flies by missing him by inches!")
+
+print(" ------------------------------ Ranged Minion ------------------------------ ")
+print("HeHeHeHe!!!...")
+
+print("------------------------------ Narrator ------------------------------")
+print("A Ranged minion slowly appears from the bush.")
+
+print(" ------------------------------ Melee Minion ------------------------------ ")
+print("Look what we have here guys, another wannabe hero who thinks he's a tough guy...")
+
+print("------------------------------ Narrator ------------------------------")
+print("The melee minion mocks Garen as he appear from behind the cannon minion.")
+
+print(" ------------------------------ Ranged Minion ------------------------------ ")
+print("This one looks like he'll be fun... do us a favor tough guy and don't embarrass us by crying like a baby.")
+
+print(" ------------------------------ Garen ------------------------------ ")
+print("Do not bother me and do not get in my way, I am on a mission to destroy the enemy nexus..., ")
+print("anyone or anything that hinders me will share the same fate...")
+
+print(" ------------------------------ Cannon Minion ------------------------------ ")
+print("You have to get through us first, so I guess you're not gunna MAKE IT!!! HAHAHAHAH!!!")
+
+print(" ------------------------------ Ranged Minion ------------------------------ ")
+print("HeHeHeHeHe!!!")
+
+print(" ------------------------------ Melee Minion ------------------------------ ")
+print("HAHAHAHAH!!!")
+
+print("------------------------------ Narrator ------------------------------")
+print("Garen has had enough! He grips his sword tighly and swings behind him. He cleanly cleaves through the bush with his")
+print("razor sharp blade leaving a perfectly precise, level cut. The ranged minion quickly dodges the blade as a sliver of")
+print("his cloak slowly falls to the ground. The ranged minion quickly joins the others as they prepare for battle.")
+
+
+
+
 
 
 
